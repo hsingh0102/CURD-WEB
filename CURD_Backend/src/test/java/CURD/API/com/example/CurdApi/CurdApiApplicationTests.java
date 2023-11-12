3fa1ca2fd73f4ed75c31d6f49005cf39425ecd4f
Hariom Singh <hariom01feb00@gmail.com>
2023-11-12 21:27:44 +0530

    add exception handling

--- CURD_Backend/src/test/java/CURD/API/com/example/CurdApi/CurdApiApplicationTests.java
+++ CURD_Backend/src/test/java/CURD/API/com/example/CurdApi/CurdApiApplicationTests.java
@@ -1,13 +1,119 @@
 package CURD.API.com.example.CurdApi;
 
+import CURD.API.com.example.CurdApi.Model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.Assert;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.util.*;
+import org.springframework.web.util.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CurdApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CurdApiApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetAllUsers() {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/users",
+				HttpMethod.GET, entity, String.class);
+
+		Assert.assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testGetAllUsersWithParameters() {
+		HttpHeaders headers = new HttpHeaders();
+
+		// Create a map for query parameters
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("param1", "value1");
+		params.add("param2", "value2");
+
+		// Add the parameters to the URL
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getRootUrl() + "/users")
+				.queryParams(params);
+
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(),
+				HttpMethod.GET, entity, String.class);
+
+		Assert.assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testGetUserById() {
+		User user = restTemplate.getForObject(getRootUrl() + "/users/1", User.class);
+		System.out.println(user.getFirstName());
+		Assert.assertNotNull(user);
+	}
+
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setEmail("admin@gmail.com");
+		user.setFirstName("admin");
+		user.setLastName("admin");
+		user.setCreatedBy("admin");
+		user.setUpdatedBy("admin");
+
+		ResponseEntity<User> postResponse = restTemplate.postForEntity(getRootUrl() + "/users", user, User.class);
+		Assert.assertNotNull(postResponse);
+		Assert.assertNotNull(postResponse.getBody());
+	}
+
+	@Test
+	public void testUpdatePost() {
+		int id = 1;
+		User user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+		user.setFirstName("admin1");
+		user.setLastName("admin2");
+
+		restTemplate.put(getRootUrl() + "/users/" + id, user);
+
+		User updatedUser = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+		Assert.assertNotNull(updatedUser);
+	}
+
+	@Test
+	public void testDeletePost() {
+		int id = 2;
+		User user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+		Assert.assertNotNull(user);
+
+		restTemplate.delete(getRootUrl() + "/users/" + id);
+
+		try {
+			user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+		} catch (final HttpClientErrorException e) {
+			Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
